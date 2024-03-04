ec3e30b44d57c48de4e3dcb71ee3ca4ef6e1932e
Ashad <ashad001sp@gmail.com>
2024-03-05 00:30:22 +0500

    boolean model v1: incomplete

--- src/boolean_model.py
+++ src/boolean_model.py
@@ -1,16 +1,48 @@
 import re
-from typing import List, Dict, Tuple, Optional, Any
+import json
+from typing import List, Dict, Tuple, Optional, Any, Set
+# Assuming the below imports work as intended
+from inverted_index import InvertedIndex
+from porter_stemmer import PorterStemmer as Stemmer
+from tokenizer import Tokenizer
+from utils import get_logger, timing_decorator
+from processor import processor
 
 class BooleanModel:
     def __init__(self) -> None:
-        self.index: Dict[str, Dict[str, List[int]]] = {}
+        processor('../data/ResearchPapers')
+        self.inv_idx = None
+        with open(f'test_inv-index.json', 'r') as f:
+            self.inv_idx = json.load(f)
+        self.stemmer = Stemmer()
+        self.tokenizer = Tokenizer()
+        self.logger = get_logger("boolean_model", see_time=True, console_log=True)
+        self.error_logger = get_logger("boolean_model_error", see_time=True, console_log=True)
     
-    def intersection(self, list1: List[int], list2: List[int]) -> List[int]:
-        """
-        finds intersection of two lists
+    @timing_decorator
+    def process_query(self, query: str) -> List[str]:
+        query = re.split(r'\s', query)
+        words = [self.stemmer.stem(word.lower()) for word in query if word.upper() not in ['AND', 'OR', 'NOT']]
+        postings = self.get_postings(words)
         
-        Args:
-            list1 (List[int]): list 1
-            list2 (List[int]): list 2
-        """
-        return list(set(list1).intersection(set(list2)))
+        
+        
+
+    def get_postings(self, words: List[str]):
+        postings = []
+        for word in words:
+            if word in self.inv_idx:
+                docs = list(self.inv_idx[word].keys())
+                docs = [int(doc.split('_')[1]) for doc in docs]
+                #! is it necessary to sort..?
+                docs.sort()
+                postings.append({word: docs})
+            else:
+                self.error_logger.error(f"Word '{word}' not found in inverted index")
+        return postings
+
+if __name__=="__main__":
+    bm = BooleanModel()
+    query = "heart AND heart"
+    docs = bm.process_query(query)
+    print(docs)
