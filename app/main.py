8a2c212dfcfceea59f55655d7a51a07257e0061e
Ashad <ashad001sp@gmail.com>
2024-03-08 11:14:30 +0500

    word seuggestor added

--- app/main.py
+++ app/main.py
@@ -17,3 +17,66 @@
 # # If you want to limit how often the value gets updated, pass `debounce` value, which
 # # will force the value to only update after that many milliseconds have passed
 # with_debounce = st_keyup("Enter a value", debounce=500, key="2")
+
+import streamlit as st
+class StreamlitApp:
+    def __init__(self):
+        self.title = "Information Retrieval System"
+        self.description = "This is a simple information retrieval system that uses the extended boolean model to search for documents in a collection of research papers."
+        self.query = ""
+        self.query_results = []
+        self.query_result = None
+        
+    def toggle_theme(self):        
+        dark = '''
+        <style>
+            .stApp {
+            background-color: black;
+            }
+        </style>
+        '''
+
+        light = '''
+        <style>
+            .stApp {
+            background-color: white;
+            }
+        </style>
+        '''
+
+        st.markdown(light, unsafe_allow_html=True)
+
+        # Create a toggle button
+        toggle = st.button("Toggle theme")
+
+        # Use a global variable to store the current theme
+        if "theme" not in st.session_state:
+            st.session_state.theme = "light"
+
+        # Change the theme based on the button state
+        if toggle:
+            if st.session_state.theme == "light":
+                st.session_state.theme = "dark"
+            else:
+                st.session_state.theme = "light"
+
+        # Apply the theme to the app
+        if st.session_state.theme == "dark":
+            st.markdown(dark, unsafe_allow_html=True)
+        else:
+            st.markdown(light, unsafe_allow_html=True)
+        
+    def main(self):
+        st.title(self.title)
+        st.write(self.description)
+        # self.toggle_theme()
+        self.query = st.text_input("Enter a query")
+        self.query_results = st.button("Search")
+        if self.query_results:
+            self.query_result = self.proximity_query(self.query)
+            st.write(self.query_result)
+        st.write("The query results will be displayed here")
+        
+if __name__=="__main__":
+    app = StreamlitApp()
+    app.main()
